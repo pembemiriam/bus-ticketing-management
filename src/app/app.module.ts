9ea7a712902c76fe563e6340bbd1e2e666ccd207
kengneruphine <ruphinekengne@gmail.com>
2018-07-07 11:37:05 +0100

    user section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,41 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router'
 import { MatCardModule, MatFormFieldModule, MatButtonModule, 
-  MatDividerModule, } from '@angular/material';
+  MatDividerModule, MatToolbarModule,MatListModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './manager/dashboard/dashboard.component';
 import { AddDestinationComponent } from './manager/add-destination/add-destination.component';
+import { WelcomeComponent } from './user/welcome/welcome.component';
+import {NavbarComponent} from './user/navbar/navbar.component';
+import {ListDestinationComponent} from './user/listDestination/list-destination.component'
+
+
 
 
 const appRoutes: Routes=[
-  {path: 'login', component: LoginComponent},
-    {path: '', redirectTo: 'login', pathMatch:'full'}
+  {path: 'navbar', component: NavbarComponent, children: [
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+           {path: 'welcome', component: WelcomeComponent},
+           {path:'listDestination', component:ListDestinationComponent}
+]}
 ]
-
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    AddDestinationComponent
+    AddDestinationComponent,
+    WelcomeComponent,
+    NavbarComponent,
+    ListDestinationComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
     BrowserModule,BrowserAnimationsModule,MatCardModule, MatFormFieldModule, MatButtonModule, 
-    MatDividerModule,
+    MatDividerModule,MatToolbarModule,MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/list-destination/list-destination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-destination',
+  templateUrl: './list-destination.component.html',
+  styleUrls: ['./list-destination.component.css']
+})
+export class ListDestinationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
